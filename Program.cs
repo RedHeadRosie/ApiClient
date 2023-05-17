4ef3bedf95b77271acfbca79675469742ccd3849
Pip Matthews <pip.matthews+github@gmail.com>
2023-05-16 21:28:19 -0400

    adding basics

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        // class Item
+        // {
+        //     public string[] message { get; set; }
+        //     public string status { get; set; }
+        // }
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync("https://dog.ceo/api/breed/australian/list");
+
+            var items = await JsonSerializer.DeserializeAsync<Item>(responseAsStream);
+
+
+            Console.WriteLine($"The following dogs fall under this category : {items.message}");
+
+        }
     }
-  }
 }
